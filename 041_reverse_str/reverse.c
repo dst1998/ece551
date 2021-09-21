d2e0ff48c0c37018c1b4bd73fc039c1524a5a7a2
sd438 <sd438@duke.edu>
2021-09-21 00:04:21 -0400

    d

--- 041_reverse_str/reverse.c
+++ 041_reverse_str/reverse.c
@@ -4,6 +4,20 @@
 
 void reverse(char * str) {
   //WRITE ME!
+  if (str != NULL) {
+    int length = 0;
+    char temp;
+    char * p = str;
+    while (*p != '\0') {
+      length++;
+      p++;
+    }
+    for (int i = 0; i < length / 2; i++) {
+      temp = str[i];
+      str[i] = str[length - 1 - i];
+      str[length - 1 - i] = temp;
+    }
+  }
 }
 
 int main(void) {
@@ -13,7 +27,8 @@ int main(void) {
   char str3[] = "Captain's log, Stardate 42523.7";
   char str4[] = "Hello, my name is Inigo Montoya.";
   char str5[] = "You can be my wingman anyday!";
-  char str6[] = "Executor Selendis! Unleash the full power of your forces! There may be no tomorrow!";
+  char str6[] = "Executor Selendis! Unleash the full power of your forces! There may be "
+                "no tomorrow!";
   char * array[] = {str0, str1, str2, str3, str4, str5, str6};
   for (int i = 0; i < 7; i++) {
     reverse(array[i]);
