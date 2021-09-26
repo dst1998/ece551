bd45caa83ec44f67adf9d13942855e434a58e911
sd438 <sd438@duke.edu>
2021-09-26 01:19:30 -0400

    s

--- 043_eval1/pandemic.c
+++ 043_eval1/pandemic.c
@@ -2,6 +2,7 @@
 
 #include "stdio.h"
 #include "stdlib.h"
+#include "string.h"
 
 country_t parseLine(char * line) {
   country_t ans;
@@ -24,25 +25,43 @@ country_t parseLine(char * line) {
 }
 
 void calcRunningAvg(unsigned * data, size_t n_days, double * avg) {
-  //char * p = data;
-  unsigned total = 0;
+  double total = 0;
   for (size_t i = 0; i < n_days - 6; i++) {
     total = 0;
-    for (size_t j = 0; j < n_days; j++) {
-      total = data[i + j];
+    for (size_t j = 0; j < 7; j++) {
+      total += data[i + j];
       // printf("%ud", data[i]);
     }
-    avg[i] = total / n_days;
+    avg[i] = total / 7;
   }
 }
 
 void calcCumulative(unsigned * data, size_t n_days, uint64_t pop, double * cum) {
-  //WRITE ME
+  unsigned total = 0;
+  for (size_t i = 0; i < n_days; i++) {
+    cum[i] = (total + data[i]) * 100000 / pop;
+    total += data[i];
+    printf("%d", total);
+  }
 }
 
 void printCountryWithMax(country_t * countries,
                          size_t n_countries,
                          unsigned ** data,
                          size_t n_days) {
-  //WRITE ME
+  unsigned temp = 0;
+  size_t x = 0;
+  char country_name[64];
+  for (size_t i = 0; i < n_countries; i++) {
+    for (size_t j = 0; j < n_days; j++) {
+      if (temp < data[i][j]) {
+        temp = data[i][j];
+        x = i;
+      }
+    }
+  }
+  unsigned number_cases = temp;
+
+  strncpy(country_name, countries[x].name, 64);
+  printf("%s has the most daily cases with %u\n", country_name, number_cases);
 }
