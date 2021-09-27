2fd137c1b964ba8c4a2d0e112b8f974a9df05f0e
sd438 <sd438@duke.edu>
2021-09-26 23:23:17 -0400

    s

--- 043_eval1/pandemic.c
+++ 043_eval1/pandemic.c
@@ -6,8 +6,24 @@
 
 country_t parseLine(char * line) {
   country_t ans;
+  ans.name[0] = '\0';
+  ans.population = 0;
   char * p = line;
   int i = 0;
+  int cnt = 0;
+  while (*p != '\0') {
+    while (*p == ',') {
+      cnt++;
+      p++;
+    }
+    p++;
+  }
+  if (cnt != 1) {
+    printf("Wrong number of ','");
+    exit(EXIT_FAILURE);
+  }
+
+  p = line;
   while (*p != ',') {
     ans.name[i] = (*p);
     // printf("%c", *p);
@@ -16,15 +32,27 @@ country_t parseLine(char * line) {
   }
   ans.name[i] = '\0';
   p++;
+  while (*p == ' ') {
+    p++;
+  }
+  if (*p < 48 || *p > 57) {
+    printf("Wrong input of population");
+    exit(EXIT_FAILURE);
+  }
   ans.population = atoi(p);
   //printf("\n");
-  // ans.name[0] = '\0';
-  //ans.population = 0;
-  //if... return EXIT_FAILURE;
   return ans;
 }
 
 void calcRunningAvg(unsigned * data, size_t n_days, double * avg) {
+  if (data == NULL) {
+    printf("Could not open file");
+    exit(EXIT_FAILURE);
+  }
+  if (n_days <= 6) {
+    printf("Wrong number of days");
+    exit(EXIT_FAILURE);
+  }
   double total = 0;
   for (size_t i = 0; i < n_days - 6; i++) {
     total = 0;
@@ -37,11 +65,19 @@ void calcRunningAvg(unsigned * data, size_t n_days, double * avg) {
 }
 
 void calcCumulative(unsigned * data, size_t n_days, uint64_t pop, double * cum) {
+  if (data == NULL) {
+    printf("Could not open file");
+    exit(EXIT_FAILURE);
+  }
+  if (!n_days || !pop) {
+    printf("Wrong number of day or population");
+    exit(EXIT_FAILURE);
+  }
   unsigned total = 0;
   for (size_t i = 0; i < n_days; i++) {
-    cum[i] = (total + data[i]) * 100000 / pop;
+    cum[i] = (total + data[i]) / (double)pop * 100000;
     total += data[i];
-    printf("%d", total);
+    // printf("%d", total);
   }
 }
 
@@ -49,16 +85,32 @@ void printCountryWithMax(country_t * countries,
                          size_t n_countries,
                          unsigned ** data,
                          size_t n_days) {
+  if (data == NULL) {
+    printf("Could not open file");
+    exit(EXIT_FAILURE);
+  }
+  if (!n_days || !n_countries) {
+    printf("Wrong number of day or country");
+    exit(EXIT_FAILURE);
+  }
   unsigned temp = 0;
   size_t x = 0;
   char country_name[64];
+  unsigned same = 0;
   for (size_t i = 0; i < n_countries; i++) {
     for (size_t j = 0; j < n_days; j++) {
+      if (temp == data[i][j]) {
+        same = temp;
+      }
       if (temp < data[i][j]) {
         temp = data[i][j];
         x = i;
       }
     }
+  }
+  if (temp == same) {
+    printf("There is a tie between at least two countries");
+    exit(EXIT_FAILURE);
   }
   unsigned number_cases = temp;
 
