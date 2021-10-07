d201a7a28a7521bc4c127e77012d32cc6ad5c8e8
sd438 <sd438@duke.edu>
2021-10-07 02:37:53 -0400

    s

--- 057_outname/outname.c
+++ 057_outname/outname.c
@@ -1,8 +1,31 @@
+#include "outname.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "outname.h"
 
 char * computeOutputFileName(const char * inputName) {
   //WRITE ME
+  const char * p = inputName;
+  int cnt = 0;
+  while (*p != '\0') {
+    cnt++;
+    p++;
+  }
+  char * outname = malloc(cnt + 8);
+  p = inputName;
+  int i = 0;
+  while (*p != '\0') {
+    outname[i++] = *p;
+    p++;
+  }
+  outname[i++] = '.';
+  outname[i++] = 'c';
+  outname[i++] = 'o';
+  outname[i++] = 'u';
+  outname[i++] = 'n';
+  outname[i++] = 't';
+  outname[i++] = 's';
+  outname[i++] = '\0';
+  return outname;
 }
