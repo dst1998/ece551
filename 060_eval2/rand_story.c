b4cc13cc595ad840bcd772bfa0eccb73ca8d9e1e
sd438 <sd438@duke.edu>
2021-10-19 21:13:48 -0400

    sd

--- 060_eval2/rand_story.c
+++ 060_eval2/rand_story.c
@@ -168,14 +168,14 @@ char * judgeBlank(char * blank,
   //char * tmp;
   const char * tmp1;
   int len = 0;
-  size_t num = atoi(blank);
-  //printf("%lu, %lu", num, usedWords->n_words);
-  //if (num < 0 || usedWords->n_words < num) {  ///////////////////////////////////
-  if (usedWords->n_words < num) {
+  //size_t num = atoi(blank);
+  //printf("%lu, %lu", num, usedWords->n_words);//////////////////
+  //if (num < 0 || usedWords->n_words < num) {
+  /*if (usedWords->n_words < num) {
     perror("Wrong int!\n");
     exit(EXIT_FAILURE);
   }
-  /*if (num != 0) {
+  if (num != 0) {
     tmp = usedWords->words[usedWords->n_words - num];
     len = 1 + strlen(tmp);
     dest = realloc(dest, len * sizeof(*dest));
@@ -187,7 +187,7 @@ char * judgeBlank(char * blank,
     dest[i] = '\0';
     return dest;
     }*/
-  //return chooseWord(blank, cats);
+  //return chooseWord(blank, cats);/////////////
   if (firstcheck == 1) {
     if (notinclude(cats, blank) == NULL) {
       perror("Do not include this category.");
