b6e6912fe57a76d89391029a4f93ff3f3dd3da00
sd438 <sd438@duke.edu>
2021-11-10 16:23:17 -0500

    sd

--- 093_eval3/page.cpp
+++ 093_eval3/page.cpp
@@ -4,43 +4,48 @@
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
 
-Page::Page() : choiceNum(0) {
+Page::Page() : choiceNum(0), win(false), lose(false) {
 }
-Page::Page(size_t cn) : choiceNum(cn) {
+Page::Page(size_t cn) : choiceNum(cn), win(false), lose(false) {
+}
+Page::~Page() {
 }
 //Read input file into a vector
-std::vector<std::string> Page::ReadIn(const char * filename) {
+void Page::ReadIn(const char * filename) {
   std::ifstream infile(filename);
   if (infile.fail()) {
-    std::cerr << "Can't open file:" << filename << std::endl;
+    std::cerr << "Can't open file: " << filename << std::endl;
     exit(EXIT_FAILURE);
   }
-  std::vector<std::string> vec;
   std::string str;
   while (!infile.eof()) {
     std::getline(infile, str);
     vec.push_back(str);
   }
+  /*for (size_t i = 0; i < vec.size(); i++) {
+    std::cout << vec[i] << std::endl;
+    }*/
 }
 
 //Parsing the text before and behind '#'
-void Page::ParsePage(std::vector<std::string> vec) {
+void Page::ParsePage() {
   //if no choices,and first line is WIN
-  if (vec[0] == "WIN") {
-    Page::WinPrint();
+  if (vec[0].compare("WIN") == 0) {
+    win = true;
   }
   //if no choices,and first line is LOSE
-  if (vec[0] == "LOSE") {
-    Page::LosePrint();
+  else if (vec[0].compare("LOSE") == 0) {
+    lose = true;
   }
-  //else:have choices in the page
+  //if have choices in the page
   else {
     //get the number of choices if choices exist on the page.
     for (size_t i = 0; i < vec.size(); i++) {
-      if (vec[i] == "#") {
+      if (vec[i][0] == '#') {
         break;
       }
       else {
@@ -48,7 +53,6 @@ void Page::ParsePage(std::vector<std::string> vec) {
       }
     }
     //get the choices vector
-    std::vector<std::string> vecChoices;
     for (size_t i = 0; i < choiceNum; i++) {
       vecChoices.push_back(vec[i]);
     }
@@ -58,7 +62,6 @@ void Page::ParsePage(std::vector<std::string> vec) {
     }
   }
   //get the text part of the page
-  std::vector<std::string> vecText;
   size_t TextStartNum;  //the row that text starts.
   if (choiceNum == 0) {
     TextStartNum = 2;
@@ -71,7 +74,7 @@ void Page::ParsePage(std::vector<std::string> vec) {
   }
 }
 //reformat the choice line
-std::string RewriteChoice(std::string str, size_t i) {
+std::string Page::RewriteChoice(std::string str, size_t i) {
   std::string::size_type pos;  //the position (index) of ":"
   if ((pos = str.find(':')) == str.npos) {
     std::cerr << "Can't find ':' in the choice line." << std::endl;
@@ -79,21 +82,38 @@ std::string RewriteChoice(std::string str, size_t i) {
   }
   else {
     //substr(a,b):index a included, index b not included.
-    if (std::stoi(str.substr(0, pos)) <= 0) {
+    int index;
+    std::stringstream ssnum;
+    std::cout
+        << str.substr(0, pos)
+        << std::endl;  ////////////////////////////////////////////////////////////////
+    ssnum << str.substr(0, pos);  //put string into sstream
+    ssnum >> index;
+    std::cout << "index=" << index
+              << std::endl;  //////////////////////////////////////////////////
+    //if what before ":"change turn into a int, exit.
+    /*if (!ssnum.good()) {
+      ssnum.clear();
+      std::cerr << "Not a valid integer!\n";
+      exit(EXIT_FAILURE);
+      }*/
+    //if int index is not strictly larger than 0, exit.
+    if (index <= 0) {
       std::cerr << "The page number is not a positive integer\n." << std::endl;
       exit(EXIT_FAILURE);
     }
     //if blank or letter exits in the num???????????????????
     //////////////////////////////////////////////////////
-    std::string str_out = std::to_string(i + 1);
-    str_out += ". ";
-    str_out += str.substr(pos);
-    return str_out;
+    std::stringstream ssout;
+    ssout << i + 1 << ". " << str.substr(pos + 1);
+    return ssout.str();
   }
 }
 //print the text of the page.
-void Page::TextPrint(std::vector<std::string> & vecText) {
-  //////////////////////////////////////////////////////////////
+void Page::TextPrint() {
+  for (size_t i = 0; i < vecText.size(); i++) {
+    std::cout << vecText[i] << std::endl;
+  }
 }
 //print if win
 void Page::WinPrint() {
@@ -104,9 +124,28 @@ void Page::LosePrint() {
   std::cout << "Sorry, you have lost. Better luck next time!\n";
 }
 //print choices
-void Page::ChoicesPrint(std::vector<std::string> & vecChoices) {
+void Page::ChoicesPrint() {
   std::cout << "\n"
             << "What would you like to do?\n"
             << "\n";
-  std::cout << "";  ///////////////////////////////////////////////
+  for (size_t i = 0; i < vecChoices.size(); i++) {
+    std::cout << vecChoices[i] << std::endl;
+  }
+}
+void Page::ReadOnePage(const char * filename) {
+  ReadIn(filename);
+  ParsePage();
+  TextPrint();
+  //if no choices,and first line is WIN
+  if (win == true) {
+    WinPrint();
+  }
+  //if no choices,and first line is LOSE
+  if (lose == true) {
+    LosePrint();
+  }
+  //else:have choices in the page
+  else {
+    ChoicesPrint();
+  }
 }
--- 093_eval3/page.hpp
+++ 093_eval3/page.hpp
@@ -4,22 +4,30 @@
 class Page {
  private:
   size_t choiceNum;
+  bool win;
+  bool lose;
+  std::vector<std::string> vec;
+  std::vector<std::string> vecText;
+  std::vector<std::string> vecChoices;
 
  public:
   Page();
   Page(size_t cn);
+  ~Page();
   //Read input file into a vector
-  std::vector<std::string> ReadIn(const char * filename);
+  void ReadIn(const char * filename);
   //Parsing the text before and behind '#'
-  void ParsePage(std::vector<std::string> vec);
+  void ParsePage();
   //Refomat the choice line.
   std ::string RewriteChoice(std::string str, size_t i);
   //print the text of the page.
-  void TextPrint(std::vector<std::string> & vecText);
+  void TextPrint();
   //print if win
   void WinPrint();
   //print if lose
   void LosePrint();
   //print choices
-  void ChoicesPrint(std::vector<std::string> & vecChoices);
+  void ChoicesPrint();
+  //integrate the methods to read one page
+  void ReadOnePage(const char * filename);
 };
