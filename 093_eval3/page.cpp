d0c9940136c4997d51cbd83be687e97d36171ca8
sd438 <sd438@duke.edu>
2021-11-10 20:33:13 -0500

    sd

--- 093_eval3/page.cpp
+++ 093_eval3/page.cpp
@@ -33,25 +33,37 @@ void Page::ReadIn(const char * filename) {
 
 //Parsing the text before and behind '#'
 void Page::ParsePage() {
-  //if no choices,and first line is WIN
+  size_t TextStartNum = 0;  //the row index that text starts.
+                            //get Textstartnum
+  for (size_t i = 0; i < vec.size(); i++) {
+    if (vec[i][0] == '#') {
+      break;
+    }
+    else {
+      TextStartNum += 1;
+    }
+  }
+  TextStartNum += 1;
+  //if first line is WIN
   if (vec[0].compare("WIN") == 0) {
     win = true;
+    if (TextStartNum != 2) {
+      std::cerr << "Wrong: More than one navigation sentence!" << std::endl;
+      exit(EXIT_FAILURE);
+    }
   }
-  //if no choices,and first line is LOSE
+  //if first line is LOSE
   else if (vec[0].compare("LOSE") == 0) {
     lose = true;
+    if (TextStartNum != 2) {
+      std::cerr << "Wrong: More than one navigation sentence!" << std::endl;
+      exit(EXIT_FAILURE);
+    }
   }
-  //if have choices in the page
+  //else: first line is a choice in the page
   else {
     //get the number of choices if choices exist on the page.
-    for (size_t i = 0; i < vec.size(); i++) {
-      if (vec[i][0] == '#') {
-        break;
-      }
-      else {
-        choiceNum += 1;
-      }
-    }
+    choiceNum = TextStartNum - 1;
     //get the choices vector
     for (size_t i = 0; i < choiceNum; i++) {
       vecChoices.push_back(vec[i]);
@@ -62,13 +74,6 @@ void Page::ParsePage() {
     }
   }
   //get the text part of the page
-  size_t TextStartNum;  //the row that text starts.
-  if (choiceNum == 0) {
-    TextStartNum = 2;
-  }
-  else {
-    TextStartNum = choiceNum + 1;
-  }
   for (size_t i = TextStartNum; i < vec.size(); i++) {
     vecText.push_back(vec[i]);
   }
@@ -76,42 +81,41 @@ void Page::ParsePage() {
 //reformat the choice line
 std::string Page::RewriteChoice(std::string str, size_t i) {
   std::string::size_type pos;  //the position (index) of ":"
+  //exit if can't find ':' in the choice line
   if ((pos = str.find(':')) == str.npos) {
     std::cerr << "Can't find ':' in the choice line." << std::endl;
     exit(EXIT_FAILURE);
   }
-  else {
-    //substr(a,b):index a included, index b not included.
-    if (str.substr(0, pos)[0] == '0') {
+  //exit if nothing in front of ':' in the choice line
+  if (pos == 0) {
+    std::cerr << "Nothing in front of ':' in the choice line." << std::endl;
+    exit(EXIT_FAILURE);
+  }
+  for (size_t j = 0; j < pos; j++) {
+    if (str[j] < '1' || str[j] > '9' || str[0] == '0') {
       std::cerr << "Not a valid integer!" << std::endl;
       exit(EXIT_FAILURE);
     }
-    int index;
-    std::stringstream ssnum;
-    /*std::cout
+  }
+  //substr(a,b):index a included, index b not included.
+  int index;
+  std::stringstream ssnum;
+  /*std::cout
         << str.substr(0, pos)
         << std::endl; */ ////////////////////////////////////////////////
-    ssnum << str.substr(0, pos);  //put string into sstream
-    ssnum >> index;
-    /*std::cout << "index=" << index
+  ssnum << str.substr(0, pos);  //put string into sstream
+  ssnum >> index;
+  /*std::cout << "index=" << index
       << std::endl; */ //////////////////////////////////////////////////
-    //if what before ":"change turn into a int, exit.
-    /*if (!ssnum.good()) {
+  //if what before ":"change turn into a int, exit.
+  /*if (!ssnum.good()) {
       ssnum.clear();
       std::cerr << "Not a valid integer!\n";
       exit(EXIT_FAILURE);
       }*/
-    //if int index is not strictly larger than 0, exit.
-    if (index <= 0) {
-      std::cerr << "The page number is not a positive integer\n." << std::endl;
-      exit(EXIT_FAILURE);
-    }
-    //if blank or letter exits in the num???????????????????
-    //////////////////////////////////////////////////////
-    std::stringstream ssout;
-    ssout << " " << i + 1 << ". " << str.substr(pos + 1);
-    return ssout.str();
-  }
+  std::stringstream ssout;
+  ssout << " " << i + 1 << ". " << str.substr(pos + 1);
+  return ssout.str();
 }
 //print the text of the page.
 void Page::TextPrint() {
